b2799f0ee0a567d59435970f330d16e8effceddb
davidcoats <david@getsafetycone.com>
2023-03-07 23:51:56 -0800

    Interim changes.

--- source/R5T.T0107/Code/Bases/Classes/CommitMessage.cs
+++ source/R5T.T0107/Code/Bases/Classes/CommitMessage.cs
@@ -6,6 +6,7 @@ namespace R5T.T0107
     /// <summary>
     /// Empty implementation as base for extension methods.
     /// </summary>
+    [Obsolete("See R5T.Z0036.CommitMessage")]
     public class CommitMessage : ICommitMessage
     {
         #region Static
--- source/R5T.T0107/Code/Bases/Interfaces/ICommitMessage.cs
+++ source/R5T.T0107/Code/Bases/Interfaces/ICommitMessage.cs
@@ -8,6 +8,7 @@ namespace R5T.T0107
     /// <summary>
     /// Base for extensions ...
     /// </summary>
+    [Obsolete("See R5T.Z0036.ICommitMessage")]
     [ExtensionMethodBaseMarker]
     public interface ICommitMessage
     {
